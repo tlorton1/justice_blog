a522cc91b17fd517524f2f011c8b3bc4898014c6
tlorton1 <tlorton1@asu.edu>
2017-07-30 19:47:23 -0700

    Initial Commit

---
+++ JusticeWebApp/Models/ContactModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JusticeWebApp.Models
+{
+    public class ContactModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
